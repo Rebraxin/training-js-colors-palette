f80ce8b3d360bfadfb518af821f31dceb2dc4a95
Rebraxin <cedric.paje@gmail.com>
2020-08-01 21:48:24 +0200

    adding chroma & range inputs

--- app.js
+++ app.js
@@ -10,13 +10,18 @@ let initialColors;
 // Functions
 
 // Color Generator
+// function generateHex() {
+//   const letters = "#0123456789ABCDEF";
+//   let hash = "#";
+//   for (let i = 0; i < 6; i++) {
+//     hash += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return hash;
+// }
+
 function generateHex() {
-  const letters = "#0123456789ABCDEF";
-  let hash = "#";
-  for (let i = 0; i < 6; i++) {
-    hash += letters[Math.floor(Math.random() * 16)];
-  }
-  return hash;
+  const hexColor = chroma.random();
+  return hexColor;
 }
 
 function randomColors() {
@@ -27,7 +32,45 @@ function randomColors() {
     // Add color to the background
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
+    // Check for contrast
+    checkTextContrast(randomColor, hexText);
+    // Initial Colorise Sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
   });
+}
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if(luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  //Scale Saturation
+  const noSat = color.set("hsl.s", 0);
+  const fullSat = color.set("hsl.s", 1);
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+  // Scale Brightness
+  const midBright = color.set("hsl.l", 0.5);
+  const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+  //Update Input Colors
+  saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(
+    0
+  )}, ${scaleSat(1)})`;
+  brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(
+    0
+  )},${scaleBright(0.5)}, ${scaleBright(1)})`;
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
 }
 
 randomColors();
